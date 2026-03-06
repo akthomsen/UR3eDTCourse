bc799bbb80e8abf342eec839741408713d9529ca
losmobilos3 <losmobilos3@gmail.com>
2026-03-06 14:51:57 +0100

    Added logging of control messages

--- DT-solution/DTservices/data_recorder/data_recorder.py
+++ DT-solution/DTservices/data_recorder/data_recorder.py
@@ -25,7 +25,7 @@ def connect_to_rabbitmq():
         print("  python -m startup.start_docker_rabbitmq")
         raise e # Throw error if server was not connected to
 
-def connect_to_influxdb(token : str = "48h4-ZpcRAI2Rgxy06uATufajcpunBH5VASPmFJ7GEePuGvOu_AFvfXZZhQSul_0uGZeaV_C6z7VgnUlexJwlw=="):
+def connect_to_influxdb(token : str = "g_q1cFD4Q3eSe-cvFdRqetoQEsoi2_Socof3rIvspMHQEGmCPcboL4kQVuuGoJmKchWqtEsoNRg4U6P6oL0Xlw=="):
     # Replace these with your InfluxDB token, organization, and bucket
     org = "ur3e"
     bucket = "ur3e"
@@ -59,13 +59,11 @@ def write_datapoint_to_influxdb(write_api, dp):
 
     # Create a point with a measurement, tag, field, and a timestamp
     point = Point("sensor_data") \
-        .tag("source", "data_recorder_service")
+        .tag("source", "data_recorder_service") \
+        .time(datetime.now(timezone.utc)) 
 
     for key, value in dp_flat.items():
         point = point.field(key, value)
-
-    point = point.time(datetime.now(timezone.utc)) # Convert to nanoseconds integer
-    print(point)
 
     # Write the point to the bucket
     try:
@@ -88,6 +86,60 @@ def subscribe_to_pt_state(rmq, write_api):
         on_message_callback=on_message_received,
     )
 
+def write_ctrl_msg_to_influxdb(write_api, ctrl_msg):
+    org = "ur3e"
+    bucket = "ur3e"
+
+    # Only save messages of type play and load_program
+    if ctrl_msg["type"] not in ["load_program", "play"]:
+        return
+
+    point = Point("ctrl_msgs") \
+        .tag("source", "data_recorder_service") \
+        .tag("msg_type", ctrl_msg["type"]) \
+        .field("msg_type", ctrl_msg["type"]) \
+        .time(datetime.now(timezone.utc), WritePrecision.NS) 
+
+
+    def flatten_array_and_add(dp: dict, dp_flat: dict, field: str):
+        for i, val in enumerate(dp[field]):
+            dp_flat[f"{field}_joint_{i}"] = val
+
+    if ctrl_msg["type"] == "load_program":
+        fields = {
+            "max_velocity": ctrl_msg["max_velocity"],
+            "acceleration": ctrl_msg["acceleration"]
+        }
+
+        ctrl_msg["joint_positions"] = ctrl_msg["joint_positions"][0] # For some reason it is nested
+
+        flatten_array_and_add(ctrl_msg, fields, "joint_positions")
+
+        for key, value in fields.items():
+            point = point.field(key, value)
+
+    # Write the point to the bucket
+    try:
+        write_api.write(bucket="ur3e", org="ur3e", record=point)
+        print("Control message written successfully.")
+    except Exception as e:
+        print(f"FAILED to write to InfluxDB: {e}")
+
+
+def subscribe_to_ctrl_signals(rmq, write_api):
+    def on_message_received(ch, method, properties, body):
+        try:
+            print("✓ State:")
+            print(body)
+            write_ctrl_msg_to_influxdb(write_api, body) # Write the point to the db
+        except Exception as e:
+            print(f"✗ Failed to decode the message: {e}")
+
+    rmq.subscribe(
+        routing_key=protocol.ROUTING_KEY_CTRL,
+        on_message_callback=on_message_received,
+    )
+
 
 if __name__ == "__main__":
     # Connect to db and rabbitmq
@@ -96,4 +148,5 @@ if __name__ == "__main__":
 
     # Listen for PT state
     subscribe_to_pt_state(rmq, write_api)
+    subscribe_to_ctrl_signals(rmq, write_api)
     rmq.start_consuming()
---
+++ communication/installation/influxdb/readme.md
@@ -0,0 +1,5 @@
+# Setup
+
+When setting up the database, use "ur3e" everywhere possible, and if this is not valid use repetitions of it, e.g. "ur3eur3e" until it is valid.
+
+In the data_recorder service, please update the token using the one provided in the interface by InfluxDB (localhost:8086).
