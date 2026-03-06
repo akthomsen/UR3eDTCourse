8687cd0d40f1a206d4a6f6a8efe5010abef84ca3
losmobilos3 <losmobilos3@gmail.com>
2026-03-06 10:35:25 +0100

    Made the start_all_services script also start an influxDB container.

--- startup/start_docker_influxdb.py
+++ startup/start_docker_influxdb.py
@@ -3,20 +3,20 @@ import requests
 from startup.utils.config import resource_file_path_w_setuptools
 from startup.utils.docker_service_starter import kill_container, start
 
-containerName = "rabbitmq-server"
+containerName = "influxdb-server"
 
 
-def start_docker_rabbitmq():
-    logFileName = "logs/rabbitmq.log"
-    dockerComposeDirectoryPath = resource_file_path_w_setuptools("communication/installation/rabbitmq")
+def start_docker_influxdb():
+    logFileName = "logs/influxdb.log"
+    dockerComposeDirectoryPath = resource_file_path_w_setuptools("communication/installation/influxdb")
     sleepTimeBetweenAttempts = 1
     maxAttempts = 10
 
     def test_connection_function():
         try:
-            r = requests.get("http://localhost:15672/api/overview", auth=('ur3e', 'ur3e'))
+            r = requests.get("http://localhost:8086/api/overview")
             if r.status_code == 200:
-                print("RabbitMQ ready:\n " + r.text)
+                print("Influxdb ready:\n " + r.text)
                 return True
         except requests.exceptions.ConnectionError as x:
             # print("RabbitMQ not ready - Exception: " + x.__class__.__name__)
@@ -29,9 +29,9 @@ def start_docker_rabbitmq():
              test_connection_function, sleepTimeBetweenAttempts, maxAttempts)
 
 
-def stop_docker_rabbitmq():
+def stop_docker_influxdb():
     kill_container(containerName)
 
 
 if __name__ == '__main__':
-    start_docker_rabbitmq()
+    start_docker_influxdb()
