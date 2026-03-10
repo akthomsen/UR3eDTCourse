607b19fdb7748cdf251237b20af3583c526f02b0
Christian Porsmose <chrodn@Christians-MacBook-Air.local>
2026-03-10 22:08:14 +0100

    foreslag om refactor, og race condition?

--- DTsolution/DTservices/RT_robot_prediction/RT_robot_prediction.py
+++ DTsolution/DTservices/RT_robot_prediction/RT_robot_prediction.py
@@ -1,76 +1,44 @@
+from pathlib import Path
+import time
+
+from utils.utils import load_config
 from communication import protocol
 from communication.rabbitmq import Rabbitmq
-from datetime import datetime, timezone
-from random import random
 from DTsolution.models.kinematic_model import KinematicModel
-
-# Needed for parallism
 import threading
 
-def connect_to_rabbitmq():
-    try:
-        rmq = Rabbitmq(
-            ip="rabbitmq-server",
-            port=5672,
-            username="ur3e",
-            password="ur3e",
-            vhost="/",
-            exchange="UR3E_AMQP",
-            type="topic",
-        )
-        rmq.connect_to_server()
-        print("✓ Connected to RabbitMQ successfully")
-        return rmq
-    except Exception as e:
-        print(f"✗ Failed to connect to RabbitMQ: {e}")
-        print("\nMake sure RabbitMQ is running. You can start it with:")
-        print("  python -m startup.start_docker_rabbitmq")
-        raise e # Throw error if server was not connected to
+model_lock = threading.Lock()
 
-
-def inject_ctrl_msg_to_model(model : KinematicModel, body):
-    if body["type"] == "load_program":
-        model.fmi2SetCommandedJointAngles(body["joint_positions"][0])
-    elif body["type"] == "play":
-        model.fmi2StartMovement()
+def inject_ctrl_msg_to_model(model : KinematicModel, body : dict):
+    with model_lock:
+        if body["type"] == "load_program":
+            model.fmi2SetCommandedJointAngles(body["joint_positions"][0])
+        elif body["type"] == "play":
+            model.fmi2StartMovement()
     
 
-def subscribe_to_ctrl_signals(rmq, model : KinematicModel):
-    def on_message_received(ch, method, properties, body):
-        try:
-            print("✓ State:")
-            print(body)
-            inject_ctrl_msg_to_model(model, body)
-        except Exception as e:
-            print(f"✗ Failed to decode the message: {e}")
+def run_simulation(model : KinematicModel, dt=0.10):
+    with model_lock:
+        i = 0
+        while True:
+            current_time = i * dt
+            model.fmi2DoStep(current_time, dt)
+            time.sleep(dt)
+            i += 1
 
-    rmq.subscribe(
-        routing_key=protocol.ROUTING_KEY_CTRL,
-        on_message_callback=on_message_received,
-    )
+def main():
+    config = load_config(Path("connect.yml"))
 
-def run_simulation(model, dt=0.10):
-    i = 0
-    while True:
-        current_time = i * dt
-        model.fmi2DoStep(current_time, dt)
-        angles = model.current_joint_angles
-        time.sleep(dt)
-        i += 1
-    
+    with Rabbitmq(**config) as rabbit_mq:
+        model = KinematicModel(movement_fidelity=1000)
+
+        rabbit_mq.subscribe(protocol.ROUTING_KEY_CTRL, lambda *_, body_json :
+                            inject_ctrl_msg_to_model(model, body_json))
+        
+        rmq_thread = threading.Thread(target=rabbit_mq.start_consuming, daemon=True)
+        rmq_thread.start()
+
+        run_simulation(model)
 
 if __name__ == "__main__":
-    # Connect to db and rabbitmq
-    rmq = connect_to_rabbitmq()
-
-    # Instantiate kinematric model
-    model = KinematicModel(movement_fidelity=1000)
-
-    # Listen for PT state
-    subscribe_to_ctrl_signals(rmq, model)
-
-    # Start new thread for consuming ctrl messages
-    rmq_thread = threading.Thread(target=rmq.start_consuming, daemon=True)
-    rmq_thread.start()
-
-    run_simulation(model)
+    main()
--- DTsolution/DTservices/data_recorder/data_recorder.py
+++ DTsolution/DTservices/data_recorder/data_recorder.py
@@ -1,46 +1,46 @@
+from pathlib import Path
+from datetime import datetime, timezone
+from functools import partial
+from typing import Callable, Any
+
 from communication import protocol
 from communication.rabbitmq import Rabbitmq
-from datetime import datetime, timezone
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
-from random import random
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS, WriteApi
+from utils.utils import load_config
 
-def connect_to_rabbitmq():
+WriterFn = Callable[[Point], Any]
+CallbackFn = Callable[[WriterFn, dict], None]
+
+
+def flatten_array_and_add(dp: dict, dp_flat: dict, field: str):
+    for i, val in enumerate(dp[field]):
+        dp_flat[f"{field}_joint_{i}"] = val
+
+
+def create_point(measurement: str, tags: dict | None = None) -> Point:
+    point = Point(measurement).time(datetime.now(timezone.utc))
+    if tags:
+        for k, v in tags.items():
+            point = point.tag(k, v)
+    return point
+
+
+def add_fields(point: Point, fields: dict) -> Point:
+    for key, value in fields.items():
+        point = point.field(key, value)
+    return point
+
+
+def safe_write(writer: WriterFn, point: Point, success_msg: str):
     try:
-        rmq = Rabbitmq(
-            ip="rabbitmq-server",
-            port=5672,
-            username="ur3e",
-            password="ur3e",
-            vhost="/",
-            exchange="UR3E_AMQP",
-            type="topic",
-        )
-        rmq.connect_to_server()
-        print("✓ Connected to RabbitMQ successfully")
-        return rmq
+        writer(record=point)
+        print(success_msg)
     except Exception as e:
-        print(f"✗ Failed to connect to RabbitMQ: {e}")
-        print("\nMake sure RabbitMQ is running. You can start it with:")
-        print("  python -m startup.start_docker_rabbitmq")
-        raise e # Throw error if server was not connected to
+        print(f"FAILED to write to InfluxDB: {e}")
 
-def connect_to_influxdb(token : str = "nIFNlhkdcuc8p7zn-zR5X0aHrMUhzEKEmcsne4YRugOvoWBTgzpeSl2SsBJp8buZJ-wR9jRXVzL7py6v23_KMw=="):
-    # Replace these with your InfluxDB token, organization, and bucket
-    org = "ur3e"
-    bucket = "ur3e"
 
-    # Initialize the client
-    client = InfluxDBClient(url="http://influxdb-server:8086", token=token, org=org)
-    write_api = client.write_api(write_options=SYNCHRONOUS)   
-    return write_api
-
-def write_datapoint_to_influxdb(write_api, dp):
-    # Replace these with your InfluxDB token, organization, and bucket
-    org = "ur3e"
-    bucket = "ur3e"
-
-    # convert flatten lists in dp
+def write_datapoint_to_influxdb(writer: WriterFn, dp: dict):
     dp_flat = {
         "robot_mode": dp["robot_mode"],
         "joint_max_speed": dp["joint_max_speed"],
@@ -48,105 +48,53 @@ def write_datapoint_to_influxdb(write_api, dp):
         "timestamp": dp["timestamp"]
     }
 
-    def flatten_array_and_add(dp: dict, dp_flat: dict, field: str):
-        for i, val in enumerate(dp[field]):
-            dp_flat[f"{field}_joint_{i}"] = val
-    
-    flatten_array_and_add(dp, dp_flat, "q_actual")
-    flatten_array_and_add(dp, dp_flat, "qd_actual")
-    flatten_array_and_add(dp, dp_flat, "q_target")
-    flatten_array_and_add(dp, dp_flat, "tcp_pose")
+    for field in ["q_actual", "qd_actual", "q_target", "tcp_pose"]:
+        flatten_array_and_add(dp, dp_flat, field)
 
-    # Create a point with a measurement, tag, field, and a timestamp
-    point = Point("sensor_data") \
-        .tag("source", "data_recorder_service") \
-        .time(datetime.now(timezone.utc)) 
+    point = create_point("sensor_data", tags={"source": "data_recorder_service"})
+    point = add_fields(point, dp_flat)
 
-    for key, value in dp_flat.items():
-        point = point.field(key, value)
+    safe_write(writer, point, "Data point written successfully.")
 
-    # Write the point to the bucket
-    try:
-        write_api.write(bucket="ur3e", org="ur3e", record=point)
-        print("Data point written successfully.")
-    except Exception as e:
-        print(f"FAILED to write to InfluxDB: {e}")
 
-def subscribe_to_pt_state(rmq, write_api):
-    def on_message_received(ch, method, properties, body):
-        try:
-            print("✓ State:")
-            print(body)
-            write_datapoint_to_influxdb(write_api, body) # Write the point to the db
-        except Exception as e:
-            print(f"✗ Failed to decode the message: {e}")
-
-    rmq.subscribe(
-        routing_key=protocol.ROUTING_KEY_STATE,
-        on_message_callback=on_message_received,
-    )
-
-def write_ctrl_msg_to_influxdb(write_api, ctrl_msg):
-    org = "ur3e"
-    bucket = "ur3e"
-
-    # Only save messages of type play and load_program
+def write_ctrl_msg_to_influxdb(writer: WriterFn, ctrl_msg: dict):
     if ctrl_msg["type"] not in ["load_program", "play"]:
         return
 
-    point = Point("ctrl_msgs") \
-        .tag("source", "data_recorder_service") \
-        .tag("msg_type", ctrl_msg["type"]) \
-        .field("msg_type", ctrl_msg["type"]) \
-        .time(datetime.now(timezone.utc)) 
-
-
-    def flatten_array_and_add(dp: dict, dp_flat: dict, field: str):
-        for i, val in enumerate(dp[field]):
-            dp_flat[f"{field}_joint_{i}"] = val
+    point = create_point("ctrl_msgs", tags={
+        "source": "data_recorder_service",
+        "msg_type": ctrl_msg["type"]
+    }).field("msg_type", ctrl_msg["type"])
 
     if ctrl_msg["type"] == "load_program":
         fields = {
             "max_velocity": ctrl_msg["max_velocity"],
             "acceleration": ctrl_msg["acceleration"]
         }
-
-        ctrl_msg["joint_positions"] = ctrl_msg["joint_positions"][0] # For some reason it is nested
-
+        ctrl_msg["joint_positions"] = ctrl_msg["joint_positions"][0]
         flatten_array_and_add(ctrl_msg, fields, "joint_positions")
+        point = add_fields(point, fields)
 
-        for key, value in fields.items():
-            point = point.field(key, value)
+    safe_write(writer, point, "Control message written successfully.")
 
-    # Write the point to the bucket
-    try:
-        write_api.write(bucket="ur3e", org="ur3e", record=point)
-        print("Control message written successfully.")
-    except Exception as e:
-        print(f"FAILED to write to InfluxDB: {e}")
+def main():
+    connect_config = load_config(Path("connect.yml"))
+    influx_config = load_config(Path("influx.yml"))
 
+    with Rabbitmq(**connect_config) as rabbit_mq, InfluxDBClient(**influx_config) as client:
+        write_api: WriteApi = client.write_api(write_options=SYNCHRONOUS)
+        writer = partial(write_api.write, bucket=influx_config["bucket"], org=influx_config["org"])
 
-def subscribe_to_ctrl_signals(rmq, write_api):
-    def on_message_received(ch, method, properties, body):
-        try:
-            print("✓ State:")
-            print(body)
-            write_ctrl_msg_to_influxdb(write_api, body) # Write the point to the db
-        except Exception as e:
-            print(f"✗ Failed to decode the message: {e}")
+        subscriptions = {
+            protocol.ROUTING_KEY_STATE: write_datapoint_to_influxdb,
+            protocol.ROUTING_KEY_CTRL: write_ctrl_msg_to_influxdb,
+        }
 
-    rmq.subscribe(
-        routing_key=protocol.ROUTING_KEY_CTRL,
-        on_message_callback=on_message_received,
-    )
+        for routing_key, func in subscriptions.items():
+            rabbit_mq.subscribe(routing_key, lambda *_, body_json, f=func: f(writer, body_json))
+
+        rabbit_mq.start_consuming()
 
 
 if __name__ == "__main__":
-    # Connect to db and rabbitmq
-    rmq = connect_to_rabbitmq()
-    write_api = connect_to_influxdb()
-
-    # Listen for PT state
-    subscribe_to_pt_state(rmq, write_api)
-    subscribe_to_ctrl_signals(rmq, write_api)
-    rmq.start_consuming()
+    main()
