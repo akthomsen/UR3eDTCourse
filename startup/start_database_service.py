f57db66ec5f70658160b5b4f158861b1dab8883d
Cl1nical <cl1nical@yahoo.com>
2026-03-17 13:53:01 +0100

    Added ability to load config file: startup.conf

---
+++ startup/start_database_service.py
@@ -0,0 +1,14 @@
+from services.db_recorder_service import DBRecorderService
+from utils.configuration import load_config
+
+def start_db_service(ok_queue=None):
+
+    recorder = DBRecorderService()
+    config = load_config("startup.conf")
+    recorder.setup(influxdb_config=config["influxdb"])
+
+    if ok_queue is not None:
+        ok_queue.put("OK")
+
+    recorder.start_recording()
+
