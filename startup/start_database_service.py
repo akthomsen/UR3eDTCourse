8841e0f02ee82721001329a3b4ddd20ac9c74ce8
Cl1nical <cl1nical@yahoo.com>
2026-03-17 18:17:58 +0100

    start db service

--- startup/start_database_service.py
+++ startup/start_database_service.py
@@ -4,7 +4,7 @@ from utils.configuration import load_config
 def start_db_service(ok_queue=None):
 
     recorder = DBRecorderService()
-    config = load_config("startup.conf")
+    config = load_config("startup/startup.conf")
     recorder.setup(influxdb_config=config["influxdb"])
 
     if ok_queue is not None:
